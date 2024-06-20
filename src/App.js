8244d23496ecc40e53636a135e8b24efab33ce30
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-20 09:42:34 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+let tg = window.Telegram.WebApp;
 
 function App() {
+  const onClose = () => {
+    tg.close();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      work <button onClick={onClose} type="button"></button>
     </div>
   );
 }
